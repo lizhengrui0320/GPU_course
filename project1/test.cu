8ded11f5eae6786caed6ccbc122e19fb67f80c54
Xiao Yang <moonlake_yhy@ouytlook.com>
2024-09-26 08:15:02 +0000

    add class layer

--- project1/test.cu
+++ project1/test.cu
@@ -141,6 +141,41 @@ __global__ void add_arrays(int* a, int* b, int* c, int size) {
  ****************************************************************************************/
 //父类：layer
 
+class Layer {
+    public:
+        Layer() {}
+        Layer(const Layer &other) = delete;
+        Layer(Layer &&other) = delete;
+        Layer &operator=(const Layer &other) = delete;
+        Layer &operator=(Layer &&other) = delete;
+
+        // connect to next layer
+        Layer &connect(Layer &next_layer) {
+            this->next = &next_layer;
+            next_layer.pre = this;
+
+            return next_layer;
+        }
+
+        virtual void forward() { throw std::runtime_error("not implement error"); };
+        virtual void backward() { throw std::runtime_error("not implement error"); };
+
+        // return pointer of weights and grads
+        virtual std::vector<std::pair<Storage *, Storage *>> parameters() {
+            throw std::runtime_error("not implement error");
+        };
+
+        virtual Storage *get_grad() { return this->grad.get(); }
+        virtual Storage *get_output() { return this->output.get(); }
+
+    protected:
+        Layer *pre;
+        Layer *next;
+
+        // inputs grad and layer output
+        std::unique_ptr<Storage> grad;
+        std::unique_ptr<Storage> output;
+};
 
 int main(int argc, char *argv[]) {
     
